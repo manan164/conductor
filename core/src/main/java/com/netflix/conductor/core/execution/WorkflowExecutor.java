68772fc6bf4ae60f9ee23e10513934beb4f0e01c
Manan <manan.bhatt@swiggy.in>
2020-07-23 11:10:51 +0530

    publishing workflow name along with task name.

--- core/src/main/java/com/netflix/conductor/core/execution/WorkflowExecutor.java
+++ core/src/main/java/com/netflix/conductor/core/execution/WorkflowExecutor.java
@@ -903,8 +903,8 @@ public class WorkflowExecutor {
         if (task.getStatus().isTerminal()) {
             long duration = getTaskDuration(0, task);
             long lastDuration = task.getEndTime() - task.getStartTime();
-            Monitors.recordTaskExecutionTime(task.getTaskDefName(), duration, true, task.getStatus());
-            Monitors.recordTaskExecutionTime(task.getTaskDefName(), lastDuration, false, task.getStatus());
+            Monitors.recordTaskExecutionTime(task.getTaskDefName(), duration, true, task.getStatus(), task.getWorkflowType());
+            Monitors.recordTaskExecutionTime(task.getTaskDefName(), lastDuration, false, task.getStatus(), task.getWorkflowType());
         }
     }
 
